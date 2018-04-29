a754c0e001e417190fd4a1dc207d1bea930fd1e2
gragog <36130475+gragog@users.noreply.github.com>
2018-04-29 13:08:59 +0200

    wrote help file

---
+++ TexHax/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TexHax
+{
+    class Helper
+    {
+        public static string NumberToFilesize(Int64 value, int decimalPlaces = 2)
+        {
+            string[] SizeSuffixes =
+                  { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+            if (value < 0) return "-" + NumberToFilesize(-value);
+
+            int i = 0;
+            decimal dValue = (decimal)value;
+            while (Math.Round(dValue, decimalPlaces) >= 1000)
+            {
+                dValue /= 1024;
+                i++;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}", dValue, SizeSuffixes[i]);
+        }
+    }
+}
